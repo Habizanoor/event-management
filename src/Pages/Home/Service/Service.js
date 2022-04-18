7cacedf629632a72f5c9d869f17ffc2463a490e8
Habizanoor <habizaboor.cse@gmail.com>
2022-04-18 16:01:02 +0600

    service done

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,28 @@
-import React from 'react';
 
-const Service = ({service}) => {
-    const {id, price, picture, name} = service;
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import './Service.css'
+
+const Service = ({ service }) => {
+    const { id, price, picture, name, description } = service;
     return (
-        <div>
-            
+        <div className='col-sm-12 col-md-6 col-lg-4 g-5'>
+            <Card>
+                <Card.Img style={{ height: "180px" }} variant="top" src={picture} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>{price}</Card.Text>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                    
+                </Card.Body>
+                <Card.Footer className='text-center' style={{ border: 'none', backgroundColor: 'white' }}>
+                    <Button variant="dark">Dark</Button>{' '}
+                </Card.Footer>
+            </Card>
         </div>
+
     );
 };
 
